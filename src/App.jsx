32173b66d96678fe8a0a48293a5a717f7173be08
Nathan Michalewicz <nathan@mwicz.com>
2023-07-11 11:16:44 -0400

    add Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { EntryModal } from "./components/site/EntryModal";
 import { Header } from "./components/site/Header";
+import { Main } from "./components/site/Main";
 
 function App() {
   return (
-    <>
+    <Main>
       <Header />
       <EntryModal />
-    </>
+    </Main>
   );
 }
 
