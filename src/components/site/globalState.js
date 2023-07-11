aa5645b301a62df7152f4689a087825effbe2784
Nathan Michalewicz <nathan@mwicz.com>
2023-07-11 11:32:15 -0400

    add viewState

--- src/components/site/globalState.js
+++ src/components/site/globalState.js
@@ -1,5 +1,16 @@
 import { atom, selector } from "recoil";
 
+export const viewState = atom({
+  key: "viewState",
+  default: {
+    longitude: 104.195,
+    latitude: 35.862,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0,
+  },
+});
+
 export const isEntryModalState = atom({
   key: "isEntryModalState",
   default: true,
