df4e43c65e166cc3fbae0f77753dd0a965b6076b
Nathan Michalewicz <nathan@mwicz.com>
2023-07-12 11:28:44 -0400

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { BaseMap } from "../components/map/BaseMap";
+import { Borders } from "../components/map/Borders";
+import { EntryModal } from "../components/site/EntryModal";
+import { Header } from "../components/site/Header";
+import { Main } from "../components/site/Main";
+export function Home() {
+  return (
+    <Main>
+      <Header />
+      <BaseMap>
+        <Borders />
+      </BaseMap>
+      <EntryModal />
+    </Main>
+  );
+}
