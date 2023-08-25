814f54cb2584c25c6e4bd7faf0a6a2df120c52c8
Nathan Michalewicz <nathan@mwicz.com>
2023-08-25 08:44:20 -0400

    foprmat

--- src/components/site/Main.jsx
+++ src/components/site/Main.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 export function Main({ children }) {
-  const MainDiv = styled.div`
-    height: 100vh;
-    width: 100vw;
-    margin: 0;
-    padding: 0;
-  `;
-  return <MainDiv>{children}</MainDiv>;
+  const MainDiv = {
+    height: "100vh",
+    width: "100vw",
+    margin: 0,
+    padding: 0,
+  };
+  return <div style={MainDiv}>{children}</div>;
 }
