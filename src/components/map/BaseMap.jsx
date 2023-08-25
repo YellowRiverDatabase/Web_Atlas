75750e5446bebce41f6faf8853f8b70c4fd0e678
Nathan Michalewicz <nathan@mwicz.com>
2023-08-25 08:43:35 -0400

    add react-map

--- src/components/map/BaseMap.jsx
+++ src/components/map/BaseMap.jsx
@@ -2,19 +2,23 @@ import DeckGL from "@deck.gl/react";
 import { useRecoilState } from "recoil";
 import { viewState } from "../site/globalState";
 import { OceansLayer } from "./OceansLayer";
+import { Map } from "react-map-gl/maplibre";
+import { BASEMAP } from "@deck.gl/carto";
 
 export function BaseMap({ children }) {
   const [view, setView] = useRecoilState(viewState);
 
   return (
     <DeckGL
-      viewState={view}
+      viewState={{ ...view }}
       style={{
         position: "relative",
       }}
       controller={true}
       layers={[OceansLayer()]}
       onViewStateChange={(e) => setView(e.viewState)}
-    />
+    >
+      <Map mapStyle={BASEMAP.VOYAGER_NOLABELS} />
+    </DeckGL>
   );
 }
