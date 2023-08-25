129724c72914c54d7c596565ccacc329b397f05f
Nathan Michalewicz <nathan@mwicz.com>
2023-08-25 08:44:57 -0400

    add timeslider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
+import { Slider } from "@mui/base";
 import { BaseMap } from "../components/map/BaseMap";
-import { Borders } from "../components/map/Borders";
 import { EntryModal } from "../components/site/EntryModal";
 import { Header } from "../components/site/Header";
 import { Main } from "../components/site/Main";
+import TimeSlider from "../components/site/TimeSlider";
 export function Home() {
   return (
     <Main>
       <Header />
       <BaseMap />
       <EntryModal />
+      <TimeSlider />
     </Main>
   );
 }
