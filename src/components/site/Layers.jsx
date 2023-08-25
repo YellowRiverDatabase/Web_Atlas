bedd39d27175104f6614376d47e6ebd2e2ea1441
Nathan Michalewicz <nathan@mwicz.com>
2023-08-25 08:44:49 -0400

    add functions

--- src/components/site/Layers.jsx
+++ src/components/site/Layers.jsx
@@ -1,5 +1,15 @@
+import { useRecoilState } from "recoil";
 import { BtnLayers } from "./BtnLayers";
+import { isLayersMenuState } from "./globalState";
+import { LayersMenu } from "./LayersMenu";
 
 export function Layers() {
-  return <BtnLayers />;
+  const [isLayersMenu, setIsLayersMenu] = useRecoilState(isLayersMenuState);
+
+  return (
+    <>
+      <BtnLayers onClick={() => setIsLayersMenu(!isLayersMenu)} />
+      {isLayersMenu && <LayersMenu />}
+    </>
+  );
 }
