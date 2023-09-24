0fa3e7c857b164303a09dce62250092ea86d59c7
Nathan Michalewicz <nathan@mwicz.com>
2023-09-24 14:48:23 -0400

    add state

--- src/components/site/globalState.js
+++ src/components/site/globalState.js
@@ -11,6 +11,11 @@ export const viewState = atom({
   },
 });
 
+export const yearsState = atom({
+  key: "yearsState",
+  default: [0, 100],
+});
+
 // state for geojson
 export const oceansState = atom({
   key: "oceansState",
@@ -41,4 +46,37 @@ export const dynastiesState = atom({
 export const erasState = atom({
   key: "erasState",
   default: {},
+});
+
+export const visibilityState = atom({
+  key: "visibilityState",
+  default: {
+    "China Borders": false,
+    Rivers: false,
+  },
+});
+
+/**
+ * state for geojson
+ */
+export const ChinaBorderState = atom({
+  key: "ChinaBorderState",
+  default: [],
+});
+
+export const riversState = atom({
+  key: "riverState",
+  default: [],
+});
+
+export const riverRoutesState = selector({
+  key: "riverRoutesState",
+  get: ({ get }) => {
+    const rivers = get(riversState);
+    const [start, end] = get(yearsState);
+    // console.log(rivers);
+    return rivers.filter((r) => {
+      return end > r.properties.yearstart && start < r.properties.yearend;
+    });
+  },
 });
