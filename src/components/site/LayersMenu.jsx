bce96150537d43deb3feb0aad94ef85fcd57da44
Nathan Michalewicz <nathan@mwicz.com>
2023-09-24 14:48:56 -0400

    fix radio btn

--- src/components/site/LayersMenu.jsx
+++ src/components/site/LayersMenu.jsx
@@ -1,19 +1,41 @@
-import { useRecoilState } from "recoil";
-import { isLayersMenuState } from "./globalState";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  dynastiesState,
+  erasState,
+  isLayersMenuState,
+  visibilityState,
+} from "./globalState";
 import { RadioBtn } from "./RadioBtn";
 
 export function LayersMenu() {
   const [isLayersMenu, setIsLayersMenu] = useRecoilState(isLayersMenuState);
+  const [visibility, setVisibility] = useRecoilState(visibilityState);
 
   return (
     <div className="menu-right">
       <div>
-        <RadioBtn
+        {Object.keys(visibility).map((key) => {
+          function handleChange(e) {
+            setVisibility({ ...visibility, [key]: e.target.checked });
+          }
+          return (
+            <RadioBtn
+              key={key}
+              value={key}
+              label={key}
+              checked={visibility[key]}
+              onChange={handleChange}
+            />
+          );
+        })}
+        {/* <RadioBtn
           value={"layers"}
-          checked={false}
-          onChange={() => console.log("I'm clicked")}
-          label={"Layers"}
-        />
+          checked={visibility.oceans}
+          onChange={(e) =>
+            setVisibility({ ...visibility, oceans: e.target.checked })
+          }
+          label={"Oceans"}
+        /> */}
       </div>
       <button onClick={() => setIsLayersMenu(false)}>Close</button>
     </div>
