0e20aa11635dc9238715d11f5ce789b2fcb897da
Nathan Michalewicz <nathan@mwicz.com>
2023-09-24 14:47:58 -0400

    fix coloring and outline

--- src/components/map/GeoJsonLayer.jsx
+++ src/components/map/GeoJsonLayer.jsx
@@ -1,32 +1,51 @@
 import { Layer, Source } from "react-map-gl";
 import { useRecoilValue } from "recoil";
-import { visibilityState } from "../site/globalState";
+import {
+  dynastiesState,
+  erasState,
+  visibilityState,
+} from "../site/globalState";
 import { useEffect } from "react";
+import { log } from "deck.gl";
 
 export function GeoJsonLayer({ visibilityName, data, setData, url, color }) {
   const visibility = useRecoilValue(visibilityState);
+  const dynasties = useRecoilValue(dynastiesState);
+  const eras = useRecoilValue(erasState);
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
-      setData(data.features);
-    };
-    fetchData();
-  }, []);
+    if (data.length === 0 && visibility[visibilityName] === true) {
+      const fetchData = async () => {
+        const res = await fetch(url);
+        const data = await res.json();
+        if (data.features.time) {
+          data.feature.start_date = dynasties[data.feature.time][0];
+          data.feature.end_date = dynasties[data.feature.time][1];
+        }
+        setData(data.features);
+      };
+      fetchData();
+    }
+  }, [data, visibility[visibilityName]]);
+  // console.log(data);
   return (
     <>
       {visibility[visibilityName]
         ? data.map((d, i) => {
             return (
-              <Source id={visibilityName + i} type="geojson" data={data[i]}>
+              <Source
+                id={visibilityName + i}
+                key={d + i}
+                type="geojson"
+                data={data[i]}
+              >
                 <Layer
                   id={visibilityName + i}
                   type="fill"
                   source={visibilityName + i}
                   paint={{
                     "fill-color": color || "lightblue",
-                    "fill-opacity": 0.8,
+                    "fill-opacity": 0.4,
+                    "fill-outline-color": "black",
                   }}
                 />
               </Source>
