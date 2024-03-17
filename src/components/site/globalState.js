f056530381a409337371d164b0c61f1327b0b3ac
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2024-03-17 19:32:43 -0400

    add state

--- src/components/site/globalState.js
+++ src/components/site/globalState.js
@@ -1,4 +1,5 @@
 import { group } from "d3-array";
+import { log } from "deck.gl";
 import { atom, selector } from "recoil";
 
 export const viewState = atom({
@@ -7,7 +8,7 @@ export const viewState = atom({
     longitude: 109.695,
     latitude: 35.862,
     zoom: 4,
-    pitch: 0,
+    pitch: 33,
     bearing: 0,
     minZoom: 5,
   },
@@ -22,6 +23,21 @@ export const yearsState = atom({
 export const oceansState = atom({
   key: "oceansState",
   default: {},
+});
+
+export const isTableState = atom({
+  key: "isTableState",
+  default: false,
+});
+
+export const tableDataState = atom({
+  key: "tableDataState",
+  default: [],
+});
+
+export const tableHeaderState = atom({
+  key: "tableHeaderState",
+  default: "",
 });
 
 // Menus and Modals
@@ -53,7 +69,6 @@ export const erasState = atom({
 export const visibilityState = atom({
   key: "visibilityState",
   default: {
-    "China Borders": false,
     "Study Area": false,
     Rivers: false,
   },
@@ -104,18 +119,40 @@ export const eventsState = atom({
   default: [],
 });
 
+export const typesState = atom({
+  key: "typesArrayState",
+  default: {},
+});
+
 export const filteredEventsState = selector({
   key: "filteredEventsState",
   get: ({ get }) => {
     const events = get(eventsState);
     const [start, end] = get(yearsState);
+    const type = get(typesState);
+    const type1 = Object.keys(type).filter((t) => type[t]);
+    const typeArray = Object.keys(type).filter((t) => type[t]);
+    console.log("typeArray", typeArray);
+    // console.log("events", events.length);
     const filtered = events.filter((e) => {
-      const evs = e.events.filter((e) => {
-        return e.en_date_start >= start && e.en_date_start <= end;
+      // console.log("events type", typeof e.events);
+      // return e.place_class !== "administrative units";
+      const parsedEvents = JSON.parse(e.events);
+      console.log("parsedEvents", typeof parsedEvents);
+      const eventsArray = parsedEvents.filter((d) => {
+        return end > d.en_date_start &&
+          start < d.en_date_start &&
+          typeArray.length > 0
+          ? typeArray.includes(d.en_type)
+          : d.en_type == d.en_type;
       });
-      return evs.length > 0;
+      const newE = { ...e, events: eventsArray };
+      return (
+        newE.events.length > 0 && newE.place_class !== "administrative units"
+      );
+      // return e.events.length > 0;
     });
-    console.log(filtered);
+    console.log("filtered: ", filtered.length);
     return filtered;
   },
 });
