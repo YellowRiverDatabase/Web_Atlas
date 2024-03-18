9aa28543a5d8a8a5f8d493810f5d072d290b2a60
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2024-03-18 08:31:17 -0400

    fix normal table operation

--- src/components/site/Table.jsx
+++ src/components/site/Table.jsx
@@ -1,9 +1,29 @@
 import { useRecoilState, useRecoilValue } from "recoil";
 import { isTableState, tableDataState, tableHeaderState } from "./globalState";
 import { useMemo } from "react";
+import { formatDate } from "../map/formatDate";
+
+// const tableStyle = {
+//   display: "flex",
+//   flexDirection: "column",
+//   position: "absolute",
+//   top: "11%",
+//   left: "5px",
+//   backgroundColor: "white",
+//   color: "black",
+//   justifyContent: "center",
+//   alignItems: "center",
+//   maxHeight: "65vh",
+//   // width: "20%",
+//   overflow: "scroll",
+//   padding: "1em",
+//   border: "1px solid black",
+//   borderRadius: "10px",
+//   // paddingTop: "200px",
+// };
 
 const tableStyle = {
-  display: "flex",
+  // display: "flex",
   flexDirection: "column",
   position: "absolute",
   top: "11%",
@@ -13,12 +33,16 @@ const tableStyle = {
   justifyContent: "center",
   alignItems: "center",
   maxHeight: "65vh",
-  // width: "20%",
-  overflow: "scroll",
   padding: "1em",
   border: "1px solid black",
   borderRadius: "10px",
-  // paddingTop: "200px",
+  overflow: "auto",
+};
+
+const tableBodyStyle = {
+  maxHeight: "60%",
+  overflow: "auto",
+  position: "relative",
 };
 
 const headerStyle = {
@@ -29,47 +53,71 @@ const headerStyle = {
   height: "3em",
 };
 
+const dataStyle = {
+  cursor: "pointer",
+  border: "1px solid black",
+  padding: "0.5em",
+};
+
+const closeButtonStyle = {
+  position: "absolute",
+  right: "10px",
+  top: "10px",
+  cursor: "pointer",
+  border: "1px solid black",
+  padding: "0.5em",
+  borderRadius: "10px",
+  backgroundColor: "lightgray",
+};
+
 export function MyTable() {
   const [isTable, setIsTable] = useRecoilState(isTableState);
   const tableData = useRecoilValue(tableDataState);
   const tableHeader = useRecoilValue(tableHeaderState);
-  // console.log(isTable);
+
+  const sortedTableData = useMemo(() => {
+    return [...tableData].sort((a, b) => +a.en_date_start - +b.en_date_start);
+  }, [tableData]);
+
+  if (!isTable) {
+    return null;
+  }
 
   return (
     <>
-      {isTable && (
-        <div style={tableStyle}>
-          <div style={headerStyle}>
-            <h1>{tableHeader}</h1>
+      <div style={tableStyle}>
+        <div style={headerStyle}>
+          <h1>{tableHeader}</h1>
+          <div style={closeButtonStyle} onClick={() => setIsTable(false)}>
+            X
           </div>
-          <table onclick={() => setIsTable(false)}>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Event</th>
-                <th>Category</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tableData.map((row, i) => (
-                <tr
-                  key={`table-row-${i}`}
-                  onClick={() => {
-                    console.log(isTable);
-                    setIsTable(false);
-                  }}
-                >
-                  <td>{row.en_date_start}</td>
-                  <td>{row.en_cat}</td>
-                  <td>{row.en_type}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
         </div>
-      )}
+        <table style={tableBodyStyle}>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Event</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedTableData.map((row, i) => (
+              <tr
+                key={`table-row-${i}`}
+                onClick={() => {
+                  console.log(isTable);
+                  setIsTable(false);
+                }}
+              >
+                <td style={dataStyle}>{formatDate(row.en_date_start)}</td>
+                <td style={dataStyle}>{row.en_cat}</td>
+                <td style={dataStyle}>{row.en_type}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      )
     </>
   );
 }
-
-// export const Table = useMemo(MyTable, [isTableState, tableDataState]);
